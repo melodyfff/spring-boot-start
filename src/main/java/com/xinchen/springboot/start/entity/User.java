9dd22ae7ddee011b3125ed2d9eac1d2457733e9f
melodyfff <307208327@qq.com>
2017-09-02 22:25:33 +0800

    [start] change to lombok @Data  instand of getter/setter

--- src/main/java/com/xinchen/springboot/start/entity/User.java
+++ src/main/java/com/xinchen/springboot/start/entity/User.java
@@ -1,5 +1,10 @@
 package com.xinchen.springboot.start.entity;
 
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
 public class User {
 
     private Long id;
@@ -8,27 +13,6 @@ public class User {
 
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
+    private Date time;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
