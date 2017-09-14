df437b6e563f595266e344c5d6fd587f98b02d56
melodyfff <307208327@qq.com>
2017-09-15 00:41:39 +0800

    [start] 模板模式处理异常 多if else的情况

--- src/main/java/com/xinchen/springboot/start/util/CommonErrorBase.java
+++ src/main/java/com/xinchen/springboot/start/util/CommonErrorBase.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 @Service
 public class CommonErrorBase implements ErrorBase {
-    private static final List<String> ERROR_LIST = Arrays.asList("密码错误","权限不足");
-
     @Override
     public void check(String msg,String errorKey) throws Exception {
         System.out.println(msg);
