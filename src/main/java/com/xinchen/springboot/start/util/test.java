700af363091d35ef56b30125f8f10229ab9c25e9
melodyfff <307208327@qq.com>
2017-09-21 22:24:44 +0800

    [start] 模板模式处理异常日常

--- src/main/java/com/xinchen/springboot/start/util/test.java
+++ src/main/java/com/xinchen/springboot/start/util/test.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class test {
-    private static final List<String> list = Arrays.asList("test1","test2","error");
+    private static final List<String> list = Arrays.asList("密码错误","权限不足","error");
     public static void main(String[] args) throws Exception {
 
 
 
 
-        String data = "<data> error1 </data>";
+        String data = "<data> 权限不足 </data>";
 
 //        if (data==null){
 //            throw new Exception("数据为空");
