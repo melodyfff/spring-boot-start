bb6905e48ec7d9d70cefeed71defe4161c46af40
melodyfff <307208327@qq.com>
2017-10-30 23:03:12 +0800

    [start] spring-data-jpa 配置文件修改

--- src/main/java/com/xinchen/springboot/start/config/CacheConfiguration.java
+++ src/main/java/com/xinchen/springboot/start/config/CacheConfiguration.java
@@ -20,8 +20,8 @@ public class CacheConfiguration {
     }
 
     /*
- * 据shared与否的设置,Spring分别通过CacheManager.create()或new CacheManager()方式来创建一个ehcache基地.
- */
+    * 据shared与否的设置,Spring分别通过CacheManager.create()或new CacheManager()方式来创建一个ehcache基地.
+    */
     @Bean
     public EhCacheManagerFactoryBean ehCacheManagerFactoryBean(){
         EhCacheManagerFactoryBean cacheManagerFactoryBean = new EhCacheManagerFactoryBean ();
