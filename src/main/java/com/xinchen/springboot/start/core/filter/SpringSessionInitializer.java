b22baebb43f18bbb449bdcc34b38ffce0c399f40
melodyfff <307208327@qq.com>
2017-11-29 17:18:35 +0800

    [start] 加入spring session redis 管理 session

---
+++ src/main/java/com/xinchen/springboot/start/core/filter/SpringSessionInitializer.java
@@ -0,0 +1,11 @@
+//package com.xinchen.springboot.start.core.filter;
+//
+//import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+//
+///**
+// * redis session 拦截器
+// * @author Xin Chen
+// * @date 2017/11/29 9:19
+// */
+//public class SpringSessionInitializer extends AbstractHttpSessionApplicationInitializer {
+//}
